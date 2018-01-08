4bb892d06aecb9b3a7876d53be9481a3015b016b
fuenkchen <sharon.funke@voodoo-arts.net>
2018-01-08 15:04:08 +0100

    Hapi update & fixture data (#56)
    * Update hapi and other dependencies
    * Add fixture data

---
+++ routes/fixtures/data.js
@@ -0,0 +1,24 @@
+const fixtureDataDirectory = '../../resources/fixtures/data';
+
+// provide every fixture data file present in ../../resources/fixtures/data
+const fixtureData = [
+  require(`${fixtureDataDirectory}/no-results-majority-images.json`),
+  require(`${fixtureDataDirectory}/no-results-no-majority-images.json`),
+  require(`${fixtureDataDirectory}/no-results-percentage-images.json`),
+  require(`${fixtureDataDirectory}/results-majority-no-images-status.json`),
+  require(`${fixtureDataDirectory}/results-majority-partly-images.json`),
+  require(`${fixtureDataDirectory}/results-no-majority-no-images.json`),
+  require(`${fixtureDataDirectory}/results-percentage-partly-images-colors.json`),
+];
+
+module.exports = {
+  path: '/fixtures/data',
+  method: 'GET',
+  options: {
+    tags: ['api'],
+    cors: true
+  },
+  handler: (request, h) => {
+    return fixtureData;
+  }
+}
